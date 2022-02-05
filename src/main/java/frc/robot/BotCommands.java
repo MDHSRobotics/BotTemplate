d54081089adf505e9f195b6992db2bb3a86e10f2
lucas frayne <fraynel.22@stu.materdei.org>
2022-02-05 14:32:00 -0800

    added swervedrive to botcommands and botsubsystems

--- src/main/java/frc/robot/BotCommands.java
+++ src/main/java/frc/robot/BotCommands.java
@@ -2,16 +2,24 @@
 package frc.robot;
 
 import frc.robot.consoles.Logger;
-
+import frc.robot.commands.swervedriver.*;
 
 // Contains singleton instances of all the commands on the robot.
 public class BotCommands {
 
+    // SwerveDrive
+    public static SwerveDrive swerveDrive;
+    public static RotateSwerveWheelsToStart rotateSwerveWheelsToStart;
+    public static ResetModulePositions resetModulePositions;
 
     // Initialize all robot commands
     public static void initializeCommands() {
         Logger.setup("Initializing BotCommands...");
 
+        // SwerveDriver
+        swerveDrive = new SwerveDrive(BotSubsystems.swerveDriver, BotControllers.xbox);
+        rotateSwerveWheelsToStart = new RotateSwerveWheelsToStart(BotSubsystems.swerveDriver);
+        resetModulePositions = new ResetModulePositions(BotSubsystems.swerveDriver);
     }
 
 }
