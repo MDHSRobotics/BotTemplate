dec064abee4806c255d3f01c3a91fdd44d3bf17c
lucas frayne <fraynel.22@stu.materdei.org>
2022-02-05 14:04:23 -0800

    ported over lots of files
    ported over botcommands, botsensors, botsubsystems, all tabs, shuffler, brains, sensors, and a few others.

---
+++ src/main/java/frc/robot/BotCommands.java
@@ -0,0 +1,17 @@
+
+package frc.robot;
+
+import frc.robot.consoles.Logger;
+
+
+// Contains singleton instances of all the commands on the robot.
+public class BotCommands {
+
+
+    // Initialize all robot commands
+    public static void initializeCommands() {
+        Logger.setup("Initializing BotCommands...");
+
+    }
+
+}
